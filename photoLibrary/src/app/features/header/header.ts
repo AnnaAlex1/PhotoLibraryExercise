9d54844f26979f494c3d91ee2aa94864537285b2
AnnaAlex1 <anna.alexandrou3@gmail.com>
2025-11-29 18:55:11 +0100

    fix: missing routing import

--- photoLibrary/src/app/features/header/header.ts
+++ photoLibrary/src/app/features/header/header.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbar } from '@angular/material/toolbar';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbar, MatButton, MatIcon],
+  imports: [RouterModule, MatToolbar, MatButton, MatIcon],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
