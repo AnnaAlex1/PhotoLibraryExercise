74e300741161ef318319b5167655c9487d0a63e9
AnnaAlex1 <anna.alexandrou3@gmail.com>
2025-11-29 17:47:43 +0100

    feat: header routing and active state button highlight

--- photoLibrary/src/app/features/header/header.spec.ts
+++ photoLibrary/src/app/features/header/header.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Header } from './header';
+import { Router } from '@angular/router';
 
 describe('Header', () => {
   let component: Header;
   let fixture: ComponentFixture<Header>;
+  let routerMock: any;
 
   beforeEach(async () => {
+    routerMock = {
+      url: '/home'      // default for test
+    };
+    
     await TestBed.configureTestingModule({
-      imports: [Header]
+      imports: [Header],
+      providers: [ {provide: Router, useValue: routerMock}]
     })
     .compileComponents();
 
@@ -19,5 +26,17 @@ describe('Header', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('isActive()', () => {
+    it('should return true when router.url matches the route', () => {
+      routerMock.url = '/home';
+      expect(component.isActive('/home')).toBeTrue();
+    });
+
+    it('should return false when router.url does not match the route', () => {
+      routerMock.url = '/';
+      expect(component.isActive('/favorite')).toBeFalse();
+    });
   });
 });
--- photoLibrary/src/app/features/header/header.ts
+++ photoLibrary/src/app/features/header/header.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbar } from '@angular/material/toolbar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,5 +11,12 @@ import { MatToolbar } from '@angular/material/toolbar';
   styleUrl: './header.scss',
 })
 export class Header {
+
+  constructor(private router: Router) {}
+
+isActive(route: string): boolean {
+  return this.router.url === route;
+}
+
 
 }
