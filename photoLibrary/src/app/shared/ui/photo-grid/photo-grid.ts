616ec6b416f5e996d863a7afc1f408762424401f
AnnaAlex1 <anna.alexandrou3@gmail.com>
2025-11-30 00:15:53 +0100

    feat: photos grid styling

--- photoLibrary/src/app/shared/ui/photo-grid/photo-grid.ts
+++ photoLibrary/src/app/shared/ui/photo-grid/photo-grid.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Photo } from '../../models/photo.model';
 import { MatGridList, MatGridTile } from '@angular/material/grid-list';
+import { MatCard } from '@angular/material/card';
 
 @Component({
   selector: 'app-photo-grid',
-  imports: [MatGridList, MatGridTile],
+  imports: [MatGridList, MatGridTile, MatCard],
   templateUrl: './photo-grid.html',
   styleUrl: './photo-grid.scss',
 })
