127ceb344f6d58fe1e5c1415b66114baee77e893
AnnaAlex1 <anna.alexandrou3@gmail.com>
2025-11-29 18:54:46 +0100

    feat: setup routing on photos-page and favorites-page

--- photoLibrary/src/app/app.routes.ts
+++ photoLibrary/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { PhotosPage } from './features/photos-page/photos-page';
+import { FavoritesPage } from './features/favorites-page/favorites-page';
+import { PhotoDetail } from './features/photo-detail/photo-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {   path: '',  component: PhotosPage   },
+    {   path: 'favorites', component: FavoritesPage    },
+    {   path: 'photo/:id', component: PhotoDetail    },
+    {   path: '**', redirectTo: ''   }
+];
