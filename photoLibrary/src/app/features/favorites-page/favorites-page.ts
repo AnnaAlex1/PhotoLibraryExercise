fd0557276cbd3ea2b706402a5ea93f204a3d1f83
AnnaAlex1 <anna.alexandrou3@gmail.com>
2025-11-30 01:27:47 +0100

    feat: use favorites service in favorites page

--- photoLibrary/src/app/features/favorites-page/favorites-page.ts
+++ photoLibrary/src/app/features/favorites-page/favorites-page.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FavoritePhotosService } from '../../core/services/favorite-photos';
+import { PhotoGrid } from '../../shared/ui/photo-grid/photo-grid';
 
 @Component({
   selector: 'app-favorites-page',
-  imports: [],
+  imports: [PhotoGrid],
   templateUrl: './favorites-page.html',
   styleUrl: './favorites-page.scss',
 })
 export class FavoritesPage {
+
+  private favoritePhotos = inject(FavoritePhotosService);
+
+  readonly favorites = this.favoritePhotos.favorites();
 
 }
