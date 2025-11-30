cc9501d2584220ebe4182713e6884e95020934eb
AnnaAlex1 <anna.alexandrou3@gmail.com>
2025-11-30 01:21:29 +0100

    feat: implement favorite photos service

---
+++ photoLibrary/src/app/core/services/favorite-photos.spec.ts
@@ -0,0 +1,80 @@
+import { TestBed } from '@angular/core/testing';
+
+import { FavoritePhotosService } from './favorite-photos';
+import { Photo } from '../../shared/models/photo.model';
+
+describe('FavoritePhotos', () => {
+  let service: FavoritePhotosService;
+
+  const photo1: Photo = { id: '1', url: 'https://picsum.photos/id/1/200/300' };
+  const photo2: Photo = { id: '2', url: 'https://picsum.photos/id/2/200/300' };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(FavoritePhotosService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('isFavorite should return true when photo is in favorites list', () => {
+    service.addToFavorites(photo1);
+    expect(service.isFavorite(photo1.id)).toBeTrue();
+  });
+
+  it('isFavorite should return false when photo is not in favorites list', () => {
+    expect(service.isFavorite(photo1.id)).toBeFalse();
+  });
+
+
+  it('should add a photo to favorites list', () => {
+    service.addToFavorites(photo1);
+
+    const favorites = service.favorites();
+    expect(favorites.length).toBe(1);
+    expect(favorites[0]).toEqual(photo1);
+  });
+
+  it('should not add the same photo twice', () => {
+    service.addToFavorites(photo1);
+    service.addToFavorites(photo1);
+
+    const favorites = service.favorites();
+    expect(favorites.length).toBe(1);
+  });
+
+  it('should remove a photo from favorites list', () => {
+    service.addToFavorites(photo1);
+    
+    service.removeFromFavorites(photo1.id);
+
+    const favorites = service.favorites();
+    expect(favorites.length).toBe(0);
+    expect(service.isFavorite(photo1.id)).toBeFalse();
+  });
+
+
+
+  it('decideAddorRemove should add when not favorite', () => {
+    service.decideAddorRemove(photo1);
+
+    const favorites = service.favorites;
+    expect(favorites.length).toBe(1);
+    expect(service.isFavorite(photo1.id)).toBeTrue();
+  
+  });
+
+  it('decideAddorRemove should remove when already favorite', () => {
+    service.addToFavorites(photo1);
+
+    service.decideAddorRemove(photo1);
+
+    const favorites = service.favorites;
+    expect(favorites.length).toBe(0);
+    expect(service.isFavorite(photo1.id)).toBeFalse();
+   
+  });
+
+
+});
---
+++ photoLibrary/src/app/core/services/favorite-photos.ts
@@ -0,0 +1,38 @@
+import { computed, Injectable, signal } from '@angular/core';
+import { Photo } from '../../shared/models/photo.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FavoritePhotosService {
+
+  private readonly _favorites = signal<Photo[]>([]); // private
+
+  readonly favorites = computed( () => this._favorites()); // publicly accessible
+
+
+  isFavorite(id: string): boolean {  // return true if exists, false otherwise
+    return this._favorites().some( ph => ph.id === id);
+  }
+
+  addToFavorites(photo: Photo): void {
+    if(this.isFavorite(photo.id)) return; // if alredy in, return
+    this._favorites.update( list => [...list, photo]);
+  }
+
+  removeFromFavorites(id: string): void {
+    this._favorites.update( list => list.filter(ph => ph.id === id));
+  }
+
+  decideAddorRemove(photo: Photo): void {
+    if (this.isFavorite(photo.id)){
+      this.removeFromFavorites(photo.id);
+    } else {
+      this.addToFavorites(photo);
+    }
+  }
+
+
+  
+
+}
