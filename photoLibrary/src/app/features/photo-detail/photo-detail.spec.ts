55c78f58063c24b8361e76d0582c201f87a8db7f
AnnaAlex1 <anna.alexandrou3@gmail.com>
2025-11-30 18:29:52 +0100

    feat: photo retrieve logic and remove from favorites logic

--- photoLibrary/src/app/features/photo-detail/photo-detail.spec.ts
+++ photoLibrary/src/app/features/photo-detail/photo-detail.spec.ts
@@ -1,23 +1,119 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PhotoDetail } from './photo-detail';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Photo } from '../../shared/models/photo.model';
+import { FavoritePhotosService } from '../../core/services/favorite-photos';
 
 describe('PhotoDetail', () => {
   let component: PhotoDetail;
   let fixture: ComponentFixture<PhotoDetail>;
 
+  let routerSpy: jasmine.SpyObj<Router>;    
+  let favoritePhotosService:  {
+    favorites: () => Photo[];
+    removeFromFavorites: jasmine.Spy;
+  };
+
+  let photoId: string | null;
+  let activatedRoute: any;
+
+  const photo: Photo = {
+    id: '56',
+    url: 'https://picsum.photos/id/56/200/300',
+  };
+
   beforeEach(async () => {
+
+    // initialization of data
+    photoId = '56';
+
+    activatedRoute = {
+      snapshot: {
+        paramMap: {
+          get: (key: string) => photoId,
+        },
+      },
+    };
+
+    routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate']);
+
+    favoritePhotosService = {
+      favorites: () => [],
+      removeFromFavorites: jasmine.createSpy('removeFromFavorites'),
+    };
+
+    // await
     await TestBed.configureTestingModule({
-      imports: [PhotoDetail]
+      imports: [PhotoDetail],
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRoute },
+        { provide: Router, useValue: routerSpy },
+        { provide: FavoritePhotosService, useValue: favoritePhotosService },
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(PhotoDetail);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  // ngOnInit
+
+  it('should load the photo when id is present', () => {
+    photoId = '56';
+    favoritePhotosService.favorites = () => [photo];
+
+    fixture.detectChanges(); // ngOnInit here
+
+    expect(component.photo).toEqual(photo);
+    expect(routerSpy.navigate).not.toHaveBeenCalled();
+  });
+  
+  it('should not load the photo when id is not present', () => {
+    photoId = '0';
+    favoritePhotosService.favorites = () => [photo];
+
+    fixture.detectChanges(); // ngOnInit here
+
+    expect(component.photo).not.toEqual(photo);
+    expect(routerSpy.navigate).toHaveBeenCalled();
+  });
+
+  it('should return to /favourites if id is null', () => {
+    photoId = null;
+
+    fixture.detectChanges(); // ngOnInit here
+
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/favorites']);
+    expect(component.photo).toBeNull();
+  });
+
+  
+  // onRemoveFromFavoritesClick
+    it('should not do anything if photo is null', () => {
+      component.photo = null;
+
+      component.onRemoveFromFavoritesClick();
+
+      expect(favoritePhotosService.removeFromFavorites).not.toHaveBeenCalled();
+      expect(routerSpy.navigate).not.toHaveBeenCalled();
+
+  });
+
+    it('should remove photo from favorites if button is clicked and photo in favorites and navigate to /favorites', () => {
+      component.photo = photo;
+
+      component.onRemoveFromFavoritesClick();
+
+      expect(favoritePhotosService.removeFromFavorites).toHaveBeenCalledWith('56');
+      expect(routerSpy.navigate).toHaveBeenCalledWith(['/favorites']);
+
+  });
+
 });
--- photoLibrary/src/app/features/photo-detail/photo-detail.ts
+++ photoLibrary/src/app/features/photo-detail/photo-detail.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Photo } from '../../shared/models/photo.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FavoritePhotosService } from '../../core/services/favorite-photos';
 
 @Component({
   selector: 'app-photo-detail',
@@ -7,5 +10,39 @@ import { Component } from '@angular/core';
   styleUrl: './photo-detail.scss',
 })
 export class PhotoDetail {
+
+  photo: Photo | null = null;
+
+  private activatedRoute = inject(ActivatedRoute);
+  private router = inject(Router);
+  private favoritesService = inject(FavoritePhotosService);
+
+  ngOnInit(): void {
+
+    // get the photo id from route
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    console.log(id);
+
+    if (!id) {
+      this.router.navigate(['/favorites']);
+      return;
+    }
+
+    // retrieve photo from favorites based on the id
+    this.photo = this.favoritesService.favorites().find(photo => photo.id === id) ?? null;
+    console.log(this.photo?.url);
+
+    if (!this.photo) {
+      this.router.navigate(['/favorites']);
+    }
+
+  }
+
+  onRemoveFromFavoritesClick(): void {
+    if (!this.photo) return;
+    this.favoritesService.removeFromFavorites(this.photo.id)
+    this.router.navigate(['/favorites']);
+  }
+
 
 }
