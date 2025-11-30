ee577271f63f133d49c21eb23157e071dc08a635
AnnaAlex1 <anna.alexandrou3@gmail.com>
2025-11-30 17:30:27 +0100

    feat: navigation to photo detail when clicking on photo

--- photoLibrary/src/app/features/favorites-page/favorites-page.spec.ts
+++ photoLibrary/src/app/features/favorites-page/favorites-page.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FavoritesPage } from './favorites-page';
+import { Photo } from '../../shared/models/photo.model';
+import { Router } from '@angular/router';
 
 describe('FavoritesPage', () => {
   let component: FavoritesPage;
   let fixture: ComponentFixture<FavoritesPage>;
+  let routerSpy: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
+    
+    routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate']);
+    
     await TestBed.configureTestingModule({
-      imports: [FavoritesPage]
+      imports: [FavoritesPage],
+      providers: [ { provide: Router, useValue: routerSpy }]
     })
     .compileComponents();
 
@@ -19,5 +26,16 @@ describe('FavoritesPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to the photo detail page when the photo is clicked', () => {
+    const photo: Photo = {
+      id: '1',
+      url: 'https://picsum.photos/id/1/200/300',
+    };
+
+    component.onPhotoClicked(photo);
+
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/photos', '1']);
   });
 });
--- photoLibrary/src/app/features/favorites-page/favorites-page.ts
+++ photoLibrary/src/app/features/favorites-page/favorites-page.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FavoritePhotosService } from '../../core/services/favorite-photos';
 import { PhotoGrid } from '../../shared/ui/photo-grid/photo-grid';
+import { Photo } from '../../shared/models/photo.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-favorites-page',
@@ -11,7 +13,13 @@ import { PhotoGrid } from '../../shared/ui/photo-grid/photo-grid';
 export class FavoritesPage {
 
   private favoritePhotos = inject(FavoritePhotosService);
+  private router = inject(Router);
 
   readonly favorites = this.favoritePhotos.favorites();
+
+  onPhotoClicked(photo: Photo){
+    this.router.navigate(['/photo', photo.id]);
+
+  }
 
 }
